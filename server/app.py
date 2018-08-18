f3d26eca07771c12f2c537a098a71e68acc51e1d
Justin Ross <jross@apache.org>
2018-08-18 10:54:22 -0700

    Checkpoint

--- server/app.py
+++ server/app.py
@@ -1,75 +1,101 @@
-import sys
-import json
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import logging as _logging
 
 from data import *
 from starlette import *
 from starlette.routing import *
 
-data = None
+_log = _logging.getLogger("data")
+
+_data = None
+_not_found = Response("Not found", 404, media_type="text/plain")
 
 @asgi_application
 async def serve_tag_index(request):
-    return JSONResponse(data.tags)
-    
+    return JSONResponse(_data.tags)
+
 @asgi_application
 async def serve_tag(request):
-    print(111, data)
-    
-    tag_id = request._scope["kwargs"]["tag_id"]
-    
+    tag_id = request["kwargs"]["tag_id"]
+
     if request.method == "GET":
-        content = data.tags[tag_id]
-        return JSONResponse(content)
+        try:
+            tag = _data.tags[tag_id]
+        except KeyError:
+            return _not_found
+
+        return JSONResponse(tag)
 
     if request.method == "PUT":
-        body = await request.body()
+        try:
+            tag = Tag(**await request.json())
+        except DataError as e:
+            return Response(f"Bad request: {e}", 400, media_type="text/plain")
 
-        with data.lock:
-            data.tags[tag_id] = json.loads(body)
+        _data.put_tag(tag)
 
-        data.modified.set()
-            
         return Response("")
 
     if request.method == "DELETE":
-        with data.lock:
-            del data.tags[tag_id]
-            
-        data.modified.set()
-            
+        try:
+            _data.delete_tag(tag_id)
+        except KeyError:
+            return _not_found
+
         return Response("")
 
 @asgi_application
 async def serve_artifact_index(request):
-    tag_id = request._scope["kwargs"]["tag_id"]
-    content = data.tags[tag_id]["artifacts"]
+    tag_id = request["kwargs"]["tag_id"]
+
+    try:
+        content = _data.tags[tag_id]["artifacts"]
+    except KeyError:
+        return _not_found
+
     return JSONResponse(content)
-        
+
 @asgi_application
 async def serve_artifact(request):
-    tag_id = request._scope["kwargs"]["tag_id"]
-    artifact_name = request._scope["kwargs"]["artifact_name"]
+    tag_id = request["kwargs"]["tag_id"]
+    artifact_name = request["kwargs"]["artifact_name"]
 
-    if request.method == "GET":
-        content = data.tags[tag_id]["artifacts"][artifact_name]
-        return JSONResponse(content)
-    
-    if request.method == "PUT":
-        raise NotImplementedError()
+    try:
+        content = _data.tags[tag_id]["artifacts"][artifact_name]
+    except KeyError:
+        return _not_found
+
+    return JSONResponse(content)
 
 if __name__ == "__main__":
     router = Router([
         Path("/api/tags", app=serve_tag_index, methods=["GET"]),
         Path("/api/tags/{tag_id}", app=serve_tag, methods=["PUT", "GET", "DELETE"]),
         Path("/api/tags/{tag_id}/artifacts", app=serve_artifact_index, methods=["GET"]),
-        Path("/api/tags/{tag_id}/artifacts/{artifact_name}", app=serve_artifact, methods=["PUT", "GET"]),
+        Path("/api/tags/{tag_id}/artifacts/{artifact_name}", app=serve_artifact, methods=["GET"]),
     ])
 
-    data = Data("data.json")
-    data.load()
-    
-    save_thread = SaveThread(data)
-    save_thread.start()
-    
+    _data = Data("data.json")
+    _data.load()
+    _data.save_thread.start()
+
     import uvicorn
     uvicorn.run(router, "0.0.0.0", 8080, log_level="warning")
--- server/data.py
+++ server/data.py
@@ -1,7 +1,29 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 import json as _json
+import logging as _logging
 import os as _os
 import threading as _threading
 import traceback as _traceback
+
+_log = _logging.getLogger("data")
 
 class Data:
     def __init__(self, file_path):
@@ -12,15 +34,21 @@ class Data:
         self.lock = _threading.Lock()
         self.modified = _threading.Event()
 
+        self.save_thread = _SaveThread(self)
+
     def load(self):
         if not _os.path.exists(self.file_path):
             return
-        
+
         with open(self.file_path, "r") as f:
             data = _json.load(f)
-            self.tags = data.get("tags")
-        
-            print(f"Loaded data from disk: {data}")
+
+            assert "tags" in data, "No tags in data"
+
+            for id, value in data["tags"].items():
+                self.tags[id] = Tag(**value)
+
+            #print(f"Loaded data from disk: {data}")
 
     def save(self):
         with self.lock:
@@ -29,14 +57,101 @@ class Data:
                 "tags": self.tags,
             }
 
-            print(f"Saving data to disk: {data}")
+            #print(f"Saving data to disk: {data}")
 
             with open(temp, "w") as f:
-                _json.dump(data, f, indent="  ")
+                _json.dump(data, f, indent=4)
 
             _os.rename(temp, self.file_path)
 
-class SaveThread(_threading.Thread):
+    def put_tag(self, tag):
+        with self.lock:
+            self.tags[tag.id] = tag
+
+        self.modified.set()
+
+    def delete_tag(self, id):
+        with self.lock:
+            del self.tags[id]
+
+        self.modified.set()
+
+class DataError(Exception):
+    pass
+
+class _DataObject(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    _fields = ()
+
+    def __init__(self, **kwargs):
+        for name, value in kwargs.items():
+            name, value = self._process_field(name, value)
+            setattr(self, name, value)
+
+        self._check_fields()
+
+    def _process_field(self, name, value):
+        if name not in self._fields:
+            raise DataError(f"Extra field '{name}'")
+
+        return name, value
+
+    def _check_fields(self):
+        for name in self._fields:
+            if name not in self:
+                raise DataError(f"Missing field '{name}'")
+
+class Tag(_DataObject):
+    _fields = "repository", "repository_url", "branch", "name", "commit", "artifacts"
+
+    def _process_field(self, name, value):
+        if name not in self._fields:
+            raise DataError(f"Extra field '{name}'")
+
+        if name == "artifacts":
+            artifacts = dict()
+
+            for iname, ivalue in value.items():
+                if "type" not in ivalue:
+                    raise DataError(f"Artifact has no type field")
+
+                cls = Artifact._subclasses_by_type[ivalue["type"]]
+                artifacts[iname] = cls(**ivalue)
+
+            return name, artifacts
+
+        return name, value
+
+    @property
+    def id(self):
+        return f"{self.repository}:{self.branch}:{self.name}"
+
+class Artifact(_DataObject):
+    _fields = "type"
+
+class ContainerImageArtifact(Artifact):
+    _fields = "type", "registry_url", "repository", "id"
+
+class MavenArtifact(Artifact):
+    _fields = "type", "repository_url", "group", "artifact", "version"
+
+class FileArtifact(Artifact):
+    _fields = "type", "url"
+
+class RpmArtifact(Artifact):
+    _fields = "type", "repository_url", "name", "version", "release"
+
+Artifact._subclasses_by_type = {
+    "container-image": ContainerImageArtifact,
+    "maven": MavenArtifact,
+    "file": FileArtifact,
+    "rpm": RpmArtifact,
+}
+
+class _SaveThread(_threading.Thread):
     def __init__(self, data):
         super().__init__()
 
