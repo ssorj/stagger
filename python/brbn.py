4d524111eb7e1bdecde045692bc440428b276e49
Justin Ross <jross@apache.org>
2019-02-15 22:36:47 +0100

    Isolate the reusable parts to a library

---
+++ python/brbn.py
@@ -0,0 +1,132 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import json.decoder as _json_decoder
+import logging as _logging
+import os as _os
+import starlette.requests as _requests
+import starlette.routing as _routing
+import starlette.staticfiles as _staticfiles
+import uuid as _uuid
+import uvicorn as _uvicorn
+
+from starlette.responses import *
+
+_log = _logging.getLogger("brbn")
+
+class Server:
+    def __init__(self, app, host="", port=8080):
+        self.app = app
+        self.host = host
+        self.port = port
+
+        self.router = Router(self.app)
+
+    def add_route(self, *args, **kwargs):
+        self.router.add_route(*args, **kwargs)
+
+    def add_static_files(self, path, dir):
+        self.router.mount(path, app=_staticfiles.StaticFiles(directory=dir))
+
+    def run(self):
+        _uvicorn.run(self.router, host=self.host, port=self.port, log_level="info")
+
+class Router(_routing.Router):
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+    def __call__(self, scope):
+        scope["app"] = self.app
+        return super().__call__(scope)
+
+class Request(_requests.Request):
+    @property
+    def app(self):
+        return self["app"]
+
+class Handler:
+    def __init__(self, scope):
+        self.scope = scope
+
+    async def __call__(self, receive, send):
+        request = Request(self.scope, receive)
+
+        try:
+            obj, response = await self.process(request)
+        except _json_decoder.JSONDecodeError as e:
+            return await BadJsonResponse(e)
+        except Exception as e:
+            return await ServerErrorResponse(e)
+
+        if response is not None:
+            return await response(receive, send)
+
+        server_etag = self.etag(request, obj)
+        client_etag = request.headers.get("If-None-Match")
+
+        if server_etag is not None and client_etag == server_etag:
+            response = NotModifiedResponse()
+        elif request.method == "HEAD":
+            response = Response("")
+        elif response is None:
+            response = await self.render(request, obj)
+
+        assert response is not None
+
+        if server_etag is not None:
+            response.headers["ETag"] = server_etag
+
+        await response(receive, send)
+
+    async def process(self, request):
+        return None, None # obj, short-circuit response
+
+    def etag(self, request, obj):
+        pass
+
+    async def render(self, request, obj):
+        pass
+
+class RedirectRequest(Exception):
+    pass
+
+class NotFoundResponse(PlainTextResponse):
+    def __init__(self, exception):
+        super().__init__(f"Not found: {exception}\n", 404)
+
+class NotModifiedResponse(PlainTextResponse):
+    def __init__(self):
+        super().__init__("Not modified\n", 304)
+
+class ServerErrorResponse(PlainTextResponse):
+    def __init__(self, exception):
+        super().__init__(f"Internal server error: {exception}\n", 500)
+
+class BadJsonResponse(PlainTextResponse):
+    def __init__(self, exception):
+        super().__init__(f"Bad request: Failure decoding JSON: {exception}\n", 400)
+
+class OkResponse(Response):
+    def __init__(self):
+        super().__init__("OK\n")
+
+class CompressedJsonResponse(Response):
+    def __init__(self, content):
+        super().__init__(content, headers={"Content-Encoding": "gzip"}, media_type="application/json")
--- python/stagger/httpserver.py
+++ python/stagger/httpserver.py
@@ -17,153 +17,75 @@
 # under the License.
 #
 
-import json as _json
 import json.decoder as _json_decoder
 import logging as _logging
 import os as _os
-import starlette.requests as _requests
-import starlette.responses as _responses
-import starlette.routing as _routing
-import starlette.staticfiles as _staticfiles
 import uuid as _uuid
-import uvicorn as _uvicorn
 
+from brbn import *
 from .model import BadDataError
 
 _log = _logging.getLogger("httpserver")
 
-class HttpServer:
+class HttpServer(Server):
     def __init__(self, app, host="", port=8080):
-        self.app = app
-        self.host = host
-        self.port = port
+        super().__init__(app, host=host, port=port)
 
-        static_dir = _os.path.join(self.app.home, "static")
+        self.add_route("/api/data",
+                       endpoint=ModelHandler, methods=["GET", "HEAD"])
+        self.add_route("/api/repos/{repo_id}",
+                       endpoint=RepoHandler, methods=["PUT", "DELETE", "GET", "HEAD"])
+        self.add_route("/api/repos/{repo_id}/branches/{branch_id}",
+                       endpoint=BranchHandler, methods=["PUT", "DELETE", "GET", "HEAD"])
+        self.add_route("/api/repos/{repo_id}/branches/{branch_id}/tags/{tag_id}",
+                       endpoint=TagHandler, methods=["PUT", "DELETE", "GET", "HEAD"])
+        self.add_route("/api/repos/{repo_id}/branches/{branch_id}/tags/{tag_id}/artifacts/{artifact_id}",
+                       endpoint=ArtifactHandler, methods=["PUT", "DELETE", "GET", "HEAD"])
+        self.add_route("/",
+                       endpoint=WebAppHandler, methods=["GET", "HEAD"])
+        self.add_route("/tags/{repo_id}/{branch_id}/{tag_id}",
+                       endpoint=WebAppHandler, methods=["GET", "HEAD"])
+        self.add_route("/artifacts/{repo_id}/{branch_id}/{tag_id}/{artifact_id}",
+                       endpoint=WebAppHandler, methods=["GET", "HEAD"])
 
-        routes = [
-            _routing.Route("/api/data",
-                           endpoint=ModelHandler, methods=["GET", "HEAD"]),
-            _routing.Route("/api/repos/{repo_id}",
-                           endpoint=RepoHandler, methods=["PUT", "DELETE", "GET", "HEAD"]),
-            _routing.Route("/api/repos/{repo_id}/branches/{branch_id}",
-                           endpoint=BranchHandler, methods=["PUT", "DELETE", "GET", "HEAD"]),
-            _routing.Route("/api/repos/{repo_id}/branches/{branch_id}/tags/{tag_id}",
-                           endpoint=TagHandler, methods=["PUT", "DELETE", "GET", "HEAD"]),
-            _routing.Route("/api/repos/{repo_id}/branches/{branch_id}/tags/{tag_id}/artifacts/{artifact_id}",
-                           endpoint=ArtifactHandler, methods=["PUT", "DELETE", "GET", "HEAD"]),
-            _routing.Route("/", endpoint=WebAppHandler, methods=["GET", "HEAD"]),
-            _routing.Route("/tags/{repo_id}/{branch_id}/{tag_id}", endpoint=WebAppHandler, methods=["GET", "HEAD"]),
-            _routing.Route("/artifacts/{repo_id}/{branch_id}/{tag_id}/{artifact_id}", endpoint=WebAppHandler, methods=["GET", "HEAD"]),
-            _routing.Mount("", app=_staticfiles.StaticFiles(directory=static_dir)),
-        ]
+        self.add_static_files("", _os.path.join(app.home, "static"))
 
-        self.router = Router(self.app, routes)
-
-    def run(self):
-        _uvicorn.run(self.router, host=self.host, port=self.port, log_level="info")
-
-class Router(_routing.Router):
-    def __init__(self, app, routes):
-        super().__init__(routes)
-        self.app = app
-
-    def __call__(self, scope):
-        scope["app"] = self.app
-        return super().__call__(scope)
-
-class NotFoundResponse(_responses.PlainTextResponse):
-    def __init__(self, exception):
-        super().__init__(f"Not found: {exception}\n", 404)
-
-class NotModifiedResponse(_responses.PlainTextResponse):
-    def __init__(self):
-        super().__init__("Not modified\n", 304)
-
-class BadJsonResponse(_responses.PlainTextResponse):
-    def __init__(self, exception):
-        super().__init__(f"Bad request: Failure decoding JSON: {exception}\n", 400)
-
-class BadDataResponse(_responses.PlainTextResponse):
+class BadDataResponse(PlainTextResponse):
     def __init__(self, exception):
         super().__init__(f"Bad request: Illegal data: {exception}\n", 400)
 
-class OkResponse(_responses.Response):
-    def __init__(self):
-        super().__init__("OK\n")
-
-class CompressedJsonResponse(_responses.Response):
-    def __init__(self, content):
-        super().__init__(content, headers={"Content-Encoding": "gzip"}, media_type="application/json")
-
-class AsgiHandler:
-    def __init__(self, scope):
-        self.scope = scope
-
-    async def __call__(self, receive, send):
-        request = _requests.Request(self.scope, receive)
-        request.app = request["app"]
-        error = None
-
-        try:
-            obj, response = await self.process(request)
-        except KeyError as e:
-            error = NotFoundResponse(e)
-        except BadDataError as e:
-            error = BadDataResponse(e)
-        except _json_decoder.JSONDecodeError as e:
-            error = BadJsonResponse(e)
-
-        if error is not None:
-            return await error(receive, send)
-
-        server_etag = self.etag(request, obj)
-        client_etag = request.headers.get("If-None-Match")
-
-        if server_etag is not None and client_etag == server_etag:
-            response = NotModifiedResponse()
-        elif request.method == "HEAD":
-            response = _responses.Response("")
-        elif response is None:
-            response = await self.render(request, obj)
-
-        assert response is not None
-
-        if server_etag is not None:
-            response.headers["ETag"] = server_etag
-
-        await response(receive, send)
-
-    async def process(self, request):
-        return None, None
-
-    def etag(self, request, obj):
-        pass
-
-    async def render(self, request, obj):
-        pass
-
-class WebAppHandler(AsgiHandler):
+class WebAppHandler(Handler):
     _etag = f'"{str(_uuid.uuid4())}"'
 
     def etag(self, request, obj):
         return self._etag
 
     async def render(self, request, obj):
-        return _responses.FileResponse(path=_os.path.join(request.app.home, "static", "index.html"))
+        return FileResponse(path=_os.path.join(request.app.home, "static", "index.html"))
 
-class ModelHandler(AsgiHandler):
+class ModelHandler(Handler):
     def etag(self, request, obj):
         return f'"{str(request.app.model.revision)}"'
+
+    async def __call__(self, receive, send):
+        try:
+            return await super().__call__(receive, send)
+        except KeyError as e:
+            return await NotFoundResponse(e)
+        except BadDataError as e:
+            return await BadDataResponse(e)
 
     async def render(self, request, obj):
         accept_encoding = request.headers.get("Accept-Encoding")
 
-        if accept_encoding is not None and "gzip" in accept_encoding:
+        if accept_encoding is not None \
+           and "gzip" in accept_encoding \
+           and request.app.model._compressed_data is not None:
             return CompressedJsonResponse(request.app.model._compressed_data)
         else:
-            return _responses.JSONResponse(request.app.model.data())
+            return JSONResponse(request.app.model.data())
 
-class ModelObjectHandler(AsgiHandler):
+class ModelObjectHandler(Handler):
     def etag(self, request, obj):
         if obj is not None:
             return f'"{str(obj._digest)}"'
@@ -176,7 +98,7 @@ class ModelObjectHandler(AsgiHandler):
         if accept_encoding is not None and "gzip" in accept_encoding:
             return CompressedJsonResponse(obj._compressed_data)
         else:
-            return _responses.JSONResponse(obj.data())
+            return JSONResponse(obj.data())
 
 class RepoHandler(ModelObjectHandler):
     async def process(self, request):
